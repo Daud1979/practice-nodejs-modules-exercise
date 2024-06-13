c6dc53a67943e86b6f34913816ef09ff295a2fe1
Daud1979 <100976611+Daud1979@users.noreply.github.com>
2024-06-13 20:27:51 +0200

    listo

--- 1-os-module.js
+++ 1-os-module.js
@@ -1,4 +1,4 @@
 // Leer README
 const { match } = require('assert');
 const os = require('os');
-console.log(`NOMBRE DEL EQUIPO: ${os.hostname()} \n Edision/Version del sistema operativo ${os.version()} \n memoria RAM total ${Math.round(os.totalmem()/1073741824)} GB`);
+console.log(`Nombre del equipo: ${os.hostname()} \nEdision/Version del sistema operativo ${os.version()} \nmemoria RAM total ${Math.round(os.totalmem()/1073741824)} GB`);
